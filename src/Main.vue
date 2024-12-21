7815bae454a013b78683d37a24e49f41c24a3c25
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2024-12-21 10:27:25 +0000

    basic layout

--- src/Main.vue
+++ src/Main.vue
@@ -4,7 +4,7 @@
     <VSystemBar>
       strust v{{ version }}
     </VSystemBar>
-    <VNavigationDrawer>
+    <VNavigationDrawer v-model="drawerOpen">
       <VListItem>
         <RouterLink to="/">Home</RouterLink>
       </VListItem>
@@ -12,24 +12,31 @@
         <RouterLink to="/calculation-tools">Calculation Tools</RouterLink>
       </VListItem>
     </VNavigationDrawer>
+    <VAppBar>
+      <VAppBarNavIcon @click="drawerOpen = !drawerOpen" v-show="!drawerOpen"/>
+      <VAppBarTitle>
+        strust
+      </VAppBarTitle>
+    </VAppBar>
     <VMain>
-      <VContainer>
-        <RouterView/>
-      </VContainer>
+      <RouterView/>
     </VMain>
   </VApp>
 </VResponsive>
 </template>
 
 <script setup lang="ts">
-import { VResponsive } from 'vuetify/components/VResponsive';
-import { VContainer } from 'vuetify/components/VGrid'
-import { VSystemBar } from 'vuetify/components/VSystemBar';
+import { VResponsive } from 'vuetify/components/VResponsive'
+import { VSystemBar } from 'vuetify/components/VSystemBar'
 import { VApp } from 'vuetify/components/VApp'
-import { VNavigationDrawer } from 'vuetify/components/VNavigationDrawer';
-import { VListItem } from 'vuetify/components/VList';
-import { VMain } from 'vuetify/components/VMain';
-import { version } from '../package.json';
+import { VNavigationDrawer } from 'vuetify/components/VNavigationDrawer'
+import { VListItem } from 'vuetify/components/VList'
+import { VAppBar, VAppBarTitle, VAppBarNavIcon } from 'vuetify/components/VAppBar'
+import { VMain } from 'vuetify/components/VMain'
+import { version } from '../package.json'
+import { ref } from 'vue'
+
+const drawerOpen = ref(null)
 </script>
 
 <style lang="scss">
