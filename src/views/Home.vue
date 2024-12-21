870cccdf89ab426ee57016993723fe627fe3b90e
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2024-12-21 10:28:10 +0000

    some cards contains feature explainations

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,34 @@
 <template>
-  <p>
-    Home
-  </p>
+  <VContainer>
+    <VCol cols="auto">
+    <VCard>
+      <VCardTitle>
+        High performance
+      </VCardTitle>
+      <VCardText>
+        powered by Webassembly.
+      </VCardText>
+    </VCard></VCol><VCol cols="auto">
+    <VCard>
+      <VCardTitle>
+        Flexible
+      </VCardTitle>
+      <VCardText>
+        open source
+      </VCardText>
+    </VCard></VCol><VCol cols="auto">
+    <VCard>
+      <VCardTitle>
+        Cross platform
+      </VCardTitle>
+      <VCardText>
+        web based system
+      </VCardText>
+    </VCard></VCol>
+  </VContainer>
 </template>
+
+<script setup lang="ts">
+import { VContainer, VCol } from 'vuetify/components/VGrid'
+import { VCard, VCardTitle, VCardText } from 'vuetify/components/VCard';
+</script>
