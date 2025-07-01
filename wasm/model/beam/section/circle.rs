f6aeb43d6d6b4714583d3392976895837b576161
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2025-07-02 00:41:33 +0900

    Drop the offset parameter from the CircleSection (#145)

--- wasm/model/beam/section/circle.rs
+++ wasm/model/beam/section/circle.rs
@@ -1,16 +1,14 @@
 use super::Section;
 use crate::Float;
-use std::array::from_fn;
 
 #[derive(Debug, Clone)]
 pub struct CircleSection {
     pub radius: Float,
-    pub offset: [Float; 2],
 }
 
 impl CircleSection {
-    pub const fn new(radius: Float, offset: [Float; 2]) -> Self {
-        Self { radius, offset }
+    pub const fn new(radius: Float) -> Self {
+        Self { radius }
     }
 }
 
@@ -19,21 +17,13 @@ impl Section for CircleSection {
         self.radius * self.radius * (180.0 as Float).to_radians()
     }
     fn centroid(&self) -> [Float; 2] {
-        self.offset
+        [Float::default(); 2]
     }
     fn moment_of_inertia(&self) -> [Float; 2] {
-        from_fn(|i| {
-            (self.radius * self.radius + self.offset[i] * self.offset[i] * 4.0)
-                * self.radius
-                * self.radius
-                * (45.0 as Float).to_radians()
-        })
+        [self.radius * self.radius * self.radius * self.radius * (45.0 as Float).to_radians(); 2]
     }
     fn product_of_inertia(&self) -> Float {
-        self.radius
-            * self.radius
-            * (180.0 as Float).to_radians()
-            * self.offset.iter().product::<Float>()
+        Float::default()
     }
 }
 
@@ -44,12 +34,12 @@ mod tests {
         use super::*;
         #[test]
         fn positive_radius() {
-            let s = CircleSection::new(3.2, [0.8, -0.1]);
+            let s = CircleSection::new(3.2);
             assert_eq!(s.area(), 3.2 * 3.2 * (180.0 as Float).to_radians());
         }
         #[test]
         fn negative_radius() {
-            let s = CircleSection::new(-3.3, [-0.8, 0.1]);
+            let s = CircleSection::new(-3.3);
             assert_eq!(s.area(), 3.3 * 3.3 * (180.0 as Float).to_radians());
         }
     }
@@ -57,47 +47,37 @@ mod tests {
         use super::*;
         #[test]
         fn positive_radius() {
-            let s = CircleSection::new(3.2, [0.8, -0.1]);
-            assert_eq!(s.centroid(), [0.8, -0.1]);
+            let s = CircleSection::new(3.2);
+            assert_eq!(s.centroid(), [0.0, 0.0]);
         }
         #[test]
         fn negative_radius() {
-            let s = CircleSection::new(-3.3, [-0.8, 0.1]);
-            assert_eq!(s.centroid(), [-0.8, 0.1]);
+            let s = CircleSection::new(-3.3);
+            assert_eq!(s.centroid(), [0.0, 0.0]);
         }
     }
     mod moment_of_inertia {
         use super::*;
         #[test]
         fn positive_radius() {
-            let s = CircleSection::new(3.2, [0.8, -0.1]);
+            let s = CircleSection::new(3.2);
             assert_eq!(
-                format!("{:.13?}", s.moment_of_inertia()),
-                format!(
-                    "{:.13?}",
-                    [
-                        6.4 * 6.4 * 6.4 * 6.4 * (180.0 as Float).to_radians() / 64.0
-                            + 3.2 * 3.2 * (180.0 as Float).to_radians() * 0.8 * 0.8,
-                        6.4 * 6.4 * 6.4 * 6.4 * (180.0 as Float).to_radians() / 64.0
-                            + 3.2 * 3.2 * (180.0 as Float).to_radians() * 0.1 * 0.1,
-                    ]
-                )
+                s.moment_of_inertia(),
+                [
+                    6.4 * 6.4 * 6.4 * 6.4 * (180.0 as Float).to_radians() / 64.0,
+                    6.4 * 6.4 * 6.4 * 6.4 * (180.0 as Float).to_radians() / 64.0,
+                ]
             );
         }
         #[test]
         fn negative_radius() {
-            let s = CircleSection::new(-3.3, [-0.8, 0.1]);
+            let s = CircleSection::new(-3.3);
             assert_eq!(
-                format!("{:.12?}", s.moment_of_inertia()),
-                format!(
-                    "{:.12?}",
-                    [
-                        6.6 * 6.6 * 6.6 * 6.6 * (180.0 as Float).to_radians() / 64.0
-                            + 3.3 * 3.3 * (180.0 as Float).to_radians() * 0.8 * 0.8,
-                        6.6 * 6.6 * 6.6 * 6.6 * (180.0 as Float).to_radians() / 64.0
-                            + 3.3 * 3.3 * (180.0 as Float).to_radians() * 0.1 * 0.1,
-                    ]
-                )
+                s.moment_of_inertia(),
+                [
+                    6.6 * 6.6 * 6.6 * 6.6 * (180.0 as Float).to_radians() / 64.0,
+                    6.6 * 6.6 * 6.6 * 6.6 * (180.0 as Float).to_radians() / 64.0,
+                ]
             );
         }
     }
@@ -105,19 +85,13 @@ mod tests {
         use super::*;
         #[test]
         fn positive_radius() {
-            let s = CircleSection::new(3.2, [-1.1, 8.2]);
-            assert_eq!(
-                s.product_of_inertia(),
-                -3.2 * 3.2 * (180.0 as Float).to_radians() * 1.1 * 8.2
-            );
+            let s = CircleSection::new(3.2);
+            assert_eq!(s.product_of_inertia(), 0.0);
         }
         #[test]
         fn negative_radius() {
-            let s = CircleSection::new(-3.3, [1.1, -8.2]);
-            assert_eq!(
-                s.product_of_inertia(),
-                -3.3 * 3.3 * (180.0 as Float).to_radians() * 1.1 * 8.2
-            );
+            let s = CircleSection::new(-3.3);
+            assert_eq!(s.product_of_inertia(), 0.0);
         }
     }
 }
