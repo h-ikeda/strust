ac037d4b3d7509a158ee359cbf0c2340ea4e9f8a
h-ikeda <ikeda_hiroki@icloud.com>
2025-07-02 13:47:27 +0000

    principal_axis around origin, not around centroid

--- wasm/model/beam/section/mod.rs
+++ wasm/model/beam/section/mod.rs
@@ -10,14 +10,8 @@ pub trait Section {
 /// Calculates the principal axis direction of the section.
 /// Returns the angle of axis in radians.
 pub fn principal_axis(section: impl Section) -> Float {
-    let area = section.area();
-    let center = section.centroid();
-    let ixy = section.product_of_inertia() - center.iter().product::<Float>() * area;
-    let mut i = section.moment_of_inertia();
-    for (v, c) in i.iter_mut().zip(center) {
-        *v -= c * c * area;
-    }
-    (ixy * -2.0).atan2(i[1] - i[0]) * 0.5
+    let [jy, jx] = section.moment_of_inertia();
+    (section.product_of_inertia() * -2.0).atan2(jx - jy) * 0.5
 }
 
 pub mod circle;
@@ -27,178 +21,163 @@ pub mod translated;
 #[cfg(test)]
 mod tests {
     use super::*;
-    mod principal_axis {
+    mod centered_rectangle {
         use super::*;
-        /// width: 4.0
-        /// height: 6.0
-        /// center: [1.2, 2.0]
-        /// axis_rotation: -15 degrees
-        struct TiltedPortraitRectangleSection {}
-        impl Section for TiltedPortraitRectangleSection {
+        // Mock a centered rectangle section.
+        // width: 4.9; height: 8.1; rotation: 13 degrees;
+        struct TestSection {}
+        impl Section for TestSection {
             fn area(&self) -> Float {
-                24.0
-            }
-            fn centroid(&self) -> [Float; 2] {
-                [1.2, 2.0]
-            }
-            fn moment_of_inertia(&self) -> [Float; 2] {
-                let r3 = (3.0 as Float).sqrt();
-                [52.0 - 10.0 * r3 + 34.56, 52.0 + 10.0 * r3 + 96.0]
-            }
-            fn product_of_inertia(&self) -> Float {
-                10.0 + 57.6
-            }
-        }
-        #[test]
-        fn tilted_portrait_rectangle_section() {
-            assert_eq!(
-                format!(
-                    "{:.13?}",
-                    principal_axis(TiltedPortraitRectangleSection {}).to_degrees()
-                ),
-                format!("{:.13?}", -15.0)
-            );
-        }
-        /// width: 4.0
-        /// height: 6.0
-        /// center: [-2.0, 1.2]
-        /// axis_rotation: 75 degrees
-        struct RotatedPortraitRectangleSection {}
-        impl Section for RotatedPortraitRectangleSection {
-            fn area(&self) -> Float {
-                24.0
-            }
-            fn centroid(&self) -> [Float; 2] {
-                [-2.0, 1.2]
-            }
-            fn moment_of_inertia(&self) -> [Float; 2] {
-                let r3 = (3.0 as Float).sqrt();
-                [52.0 + 10.0 * r3 + 96.0, 52.0 - 10.0 * r3 + 34.56]
-            }
-            fn product_of_inertia(&self) -> Float {
-                -10.0 - 57.6
-            }
-        }
-        #[test]
-        fn rotated_portrait_rectangle_section() {
-            assert_eq!(
-                format!(
-                    "{:.13?}",
-                    principal_axis(RotatedPortraitRectangleSection {}).to_degrees()
-                ),
-                format!("{:.13?}", 75.0)
-            );
-        }
-        /// width: 6.0
-        /// height: 4.0
-        /// center: [2.0, 1.2]
-        /// axis_rotation: 15 degrees
-        struct TiltedLandscapeRectangleSection {}
-        impl Section for TiltedLandscapeRectangleSection {
-            fn area(&self) -> Float {
-                24.0
-            }
-            fn centroid(&self) -> [Float; 2] {
-                [2.0, 1.2]
-            }
-            fn moment_of_inertia(&self) -> [Float; 2] {
-                let r3 = (3.0 as Float).sqrt();
-                [52.0 + 10.0 * r3 + 96.0, 52.0 - 10.0 * r3 + 34.56]
-            }
-            fn product_of_inertia(&self) -> Float {
-                10.0 + 57.6
-            }
-        }
-        #[test]
-        fn tilted_landscape_rectangle_section() {
-            assert_eq!(
-                format!(
-                    "{:.13?}",
-                    principal_axis(TiltedLandscapeRectangleSection {}).to_degrees()
-                ),
-                format!("{:.13?}", -75.0)
-            );
-        }
-        /// width: 6.0
-        /// height: 4.0
-        /// center: [-1.2, 2.0]
-        /// axis_rotation: -75 degrees
-        struct RotatedLandscapeRectangleSection {}
-        impl Section for RotatedLandscapeRectangleSection {
-            fn area(&self) -> Float {
-                24.0
-            }
-            fn centroid(&self) -> [Float; 2] {
-                [-1.2, 2.0]
-            }
-            fn moment_of_inertia(&self) -> [Float; 2] {
-                let r3 = (3.0 as Float).sqrt();
-                [52.0 - 10.0 * r3 + 34.56, 52.0 + 10.0 * r3 + 96.0]
-            }
-            fn product_of_inertia(&self) -> Float {
-                -10.0 - 57.6
-            }
-        }
-        #[test]
-        fn rotated_landscape_rectangle_section() {
-            assert_eq!(
-                format!(
-                    "{:.13?}",
-                    principal_axis(RotatedLandscapeRectangleSection {}).to_degrees()
-                ),
-                format!("{:.13?}", 15.0)
-            );
-        }
-        /// width: 6.0
-        /// height: 4.0
-        /// center: [0.0, 0.0]
-        /// axis_rotation: 0 degrees
-        struct UprightPortraitRectangleSection {}
-        impl Section for UprightPortraitRectangleSection {
-            fn area(&self) -> Float {
-                24.0
+                4.9 * 8.1
             }
             fn centroid(&self) -> [Float; 2] {
                 [0.0, 0.0]
             }
             fn moment_of_inertia(&self) -> [Float; 2] {
-                [32.0, 72.0]
+                [
+                    (4.9 * 8.1 * 8.1 * 8.1 / 12.0 + 8.1 * 4.9 * 4.9 * 4.9 / 12.0) / 2.0
+                        - (4.9 * 8.1 * 8.1 * 8.1 / 12.0 - 8.1 * 4.9 * 4.9 * 4.9 / 12.0) / 2.0
+                            * ((13.0 as Float).to_radians() * 2.0).cos(),
+                    (4.9 * 8.1 * 8.1 * 8.1 / 12.0 + 8.1 * 4.9 * 4.9 * 4.9 / 12.0) / 2.0
+                        + (4.9 * 8.1 * 8.1 * 8.1 / 12.0 - 8.1 * 4.9 * 4.9 * 4.9 / 12.0) / 2.0
+                            * ((13.0 as Float).to_radians() * 2.0).cos(),
+                ]
             }
             fn product_of_inertia(&self) -> Float {
-                0.0
+                -(4.9 * 8.1 * 8.1 * 8.1 / 12.0 - 8.1 * 4.9 * 4.9 * 4.9 / 12.0) / 2.0
+                    * ((13.0 as Float).to_radians() * 2.0).sin()
             }
         }
         #[test]
-        fn upright_portrait_rectangle_section() {
-            assert_eq!(
-                principal_axis(UprightPortraitRectangleSection {}).to_degrees(),
-                0.0
-            );
+        fn principal_axis() {
+            assert_eq!(super::principal_axis(TestSection {}).to_degrees(), 13.0);
         }
-        /// width: 4.0
-        /// height: 6.0
-        /// center: [0.0, 0.0]
-        /// axis_rotation: 0 degrees
-        struct UprightLandscapeRectangleSection {}
-        impl Section for UprightLandscapeRectangleSection {
+    }
+    mod first_quadrant_circle {
+        use super::*;
+        // Mock a translated circle section.
+        // radius: 5.1; centroid: [3.4, 9.0];
+        struct TestSection {}
+        impl Section for TestSection {
             fn area(&self) -> Float {
-                24.0
+                5.1 * 5.1 * (180.0 as Float).to_radians()
             }
             fn centroid(&self) -> [Float; 2] {
-                [0.0, 0.0]
+                [3.4, 9.0]
             }
             fn moment_of_inertia(&self) -> [Float; 2] {
-                [72.0, 32.0]
+                [
+                    5.1 * 5.1 * 5.1 * 5.1 * 16.0 * (180.0 as Float).to_radians() / 64.0
+                        + 3.4 * 3.4 * 5.1 * 5.1 * (180.0 as Float).to_radians(),
+                    5.1 * 5.1 * 5.1 * 5.1 * 16.0 * (180.0 as Float).to_radians() / 64.0
+                        + 9.0 * 9.0 * 5.1 * 5.1 * (180.0 as Float).to_radians(),
+                ]
             }
             fn product_of_inertia(&self) -> Float {
-                0.0
+                5.1 * 5.1 * (180.0 as Float).to_radians() * 3.4 * 9.0
             }
         }
         #[test]
-        fn upright_landscape_rectangle_section() {
+        fn principal_axis() {
             assert_eq!(
-                principal_axis(UprightLandscapeRectangleSection {}).to_degrees(),
-                -90.0
+                super::principal_axis(TestSection {}),
+                (-3.4 as Float).atan2(9.0)
+            );
+        }
+    }
+    mod second_quadrant_circle {
+        use super::*;
+        // Mock a translated circle section.
+        // radius: 5.1; centroid: [-9.0, 3.4];
+        struct TestSection {}
+        impl Section for TestSection {
+            fn area(&self) -> Float {
+                5.1 * 5.1 * (180.0 as Float).to_radians()
+            }
+            fn centroid(&self) -> [Float; 2] {
+                [-9.0, 3.4]
+            }
+            fn moment_of_inertia(&self) -> [Float; 2] {
+                [
+                    5.1 * 5.1 * 5.1 * 5.1 * 16.0 * (180.0 as Float).to_radians() / 64.0
+                        + 9.0 * 9.0 * 5.1 * 5.1 * (180.0 as Float).to_radians(),
+                    5.1 * 5.1 * 5.1 * 5.1 * 16.0 * (180.0 as Float).to_radians() / 64.0
+                        + 3.4 * 3.4 * 5.1 * 5.1 * (180.0 as Float).to_radians(),
+                ]
+            }
+            fn product_of_inertia(&self) -> Float {
+                -5.1 * 5.1 * (180.0 as Float).to_radians() * 3.4 * 9.0
+            }
+        }
+        #[test]
+        fn principal_axis() {
+            assert_eq!(
+                super::principal_axis(TestSection {}),
+                (9.0 as Float).atan2(3.4)
+            );
+        }
+    }
+    mod third_quadrant_circle {
+        use super::*;
+        // Mock a translated circle section.
+        // radius: 5.1; centroid: [-3.4, -9.0];
+        struct TestSection {}
+        impl Section for TestSection {
+            fn area(&self) -> Float {
+                5.1 * 5.1 * (180.0 as Float).to_radians()
+            }
+            fn centroid(&self) -> [Float; 2] {
+                [-3.4, -9.0]
+            }
+            fn moment_of_inertia(&self) -> [Float; 2] {
+                [
+                    5.1 * 5.1 * 5.1 * 5.1 * 16.0 * (180.0 as Float).to_radians() / 64.0
+                        + 3.4 * 3.4 * 5.1 * 5.1 * (180.0 as Float).to_radians(),
+                    5.1 * 5.1 * 5.1 * 5.1 * 16.0 * (180.0 as Float).to_radians() / 64.0
+                        + 9.0 * 9.0 * 5.1 * 5.1 * (180.0 as Float).to_radians(),
+                ]
+            }
+            fn product_of_inertia(&self) -> Float {
+                5.1 * 5.1 * (180.0 as Float).to_radians() * 3.4 * 9.0
+            }
+        }
+        #[test]
+        fn principal_axis() {
+            assert_eq!(
+                super::principal_axis(TestSection {}),
+                (-3.4 as Float).atan2(9.0)
+            );
+        }
+    }
+    mod fourth_quadrant_circle {
+        use super::*;
+        // Mock a translated circle section.
+        // radius: 5.1; centroid: [9.0, -3.4];
+        struct TestSection {}
+        impl Section for TestSection {
+            fn area(&self) -> Float {
+                5.1 * 5.1 * (180.0 as Float).to_radians()
+            }
+            fn centroid(&self) -> [Float; 2] {
+                [9.0, -3.4]
+            }
+            fn moment_of_inertia(&self) -> [Float; 2] {
+                [
+                    5.1 * 5.1 * 5.1 * 5.1 * 16.0 * (180.0 as Float).to_radians() / 64.0
+                        + 9.0 * 9.0 * 5.1 * 5.1 * (180.0 as Float).to_radians(),
+                    5.1 * 5.1 * 5.1 * 5.1 * 16.0 * (180.0 as Float).to_radians() / 64.0
+                        + 3.4 * 3.4 * 5.1 * 5.1 * (180.0 as Float).to_radians(),
+                ]
+            }
+            fn product_of_inertia(&self) -> Float {
+                -5.1 * 5.1 * (180.0 as Float).to_radians() * 3.4 * 9.0
+            }
+        }
+        #[test]
+        fn principal_axis() {
+            assert_eq!(
+                super::principal_axis(TestSection {}),
+                (9.0 as Float).atan2(3.4)
             );
         }
     }
